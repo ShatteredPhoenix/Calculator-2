e0070203f840d3c96322406ae6f2d56bec8883c5
Humza Ahmed <ShatteredPheonix@eroam-158-139.wifi.wmin.ac.uk>
2015-02-11 11:39:43 +0000

    Initial Commit

---
+++ Calculator/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Calculator
+//
+//  Created by Humza Ahmed on 11/02/2015.
+//  Copyright (c) 2015 Humza Ahmed. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Calculator/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  Calculator
+//
+//  Created by Humza Ahmed on 11/02/2015.
+//  Copyright (c) 2015 Humza Ahmed. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
